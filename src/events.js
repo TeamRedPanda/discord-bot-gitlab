2898b169d2e3567f7d30cd2970f6cfc73e1a71ca
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2018-12-08 19:16:35 -0200

    Change push event handling to match Gitlab format

--- src/events.js
+++ src/events.js
@@ -122,26 +122,28 @@ export default class Events {
 
   static push(data) {
     let message = "";
-    const repo = data.repository.full_name;
+    const repo = data.project.path_with_namespace;
     const branch = data.ref.split("/")[2];
     if (data.commits.length === 1) {
       const commit = data.commits[0];
       const name = commit.author.name;
       const commitMessage = commit.message;
       const sha = commit.id.substring(0, 7);
-      const url = `https://github.com/${repo}/commit/${sha}`;
+      const url = commit.url;
       message += `[**${repo}:${branch}**] 1 new commit by ${name}`;
-      message += `\n${commitMessage} - ${name}`;
+      message += `\n${commitMessage}`;
       message += `\n${url}`;
     } else {
       const commits = data.commits;
 
-      message += `[**${repo}:${branch}**] ${commits.length} new commits`;
+      // NOTE: This count is the real amount of commits, but we only receive the last 20.
+      // https://docs.gitlab.com/ee/user/project/integrations/webhooks.html
+      message += `[**${repo}:${branch}**] ${data.total_commits_count} new commits`;
 
       for (let commit of commits) {
         const sha = commit.id.substring(0, 7);
-        const url = `https://github.com/${repo}/commit/${sha}`;
-        message += `\n${commit.message} - ${commit.author.name}`;
+        const url = commit.url;
+        message += `\n${commit.message} \n- ${commit.author.name}`;
         message += `\n${url}`;
       }
     }
